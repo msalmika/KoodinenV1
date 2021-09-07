125f8c1ec99ce8916bda0c8d1c872eae95dfb829
J-Niklander <johannan_aw@outlook.com>
2021-09-07 10:03:07 +0300

    oppitunticontroller

---
+++ Controllers/OppituntiController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KoodinenV1.Controllers
+{
+    public class OppituntiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+
+
+
+}
