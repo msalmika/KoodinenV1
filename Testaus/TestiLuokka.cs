87f3d9a9c01b00f717b8681e0615620247df9b8a
Marko S <msalmikangas@gmail.com>
2021-09-08 13:53:32 +0300

    Marko is back in town

---
+++ Testaus/TestiLuokka.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace KoodinenV1.Testaus
+{
+    public class TestiLuokka
+    {
+
+        const string template = @"
+        public class ScriptedClass
+            {
+                public string DoPrint()
+                {
+                    @code
+                }
+            }";
+        public static string TestaaKoodi(string syöte)
+        {
+            string data = "Terve mualima!";
+            Console.WriteLine($"Kirjoita metodin koodi joka palauttaa tekstin {data} paluuarvona");
+
+            string code = template.Replace("@code", syöte);
+            CSharpScriptEngine.Execute(code);
+            var ret = CSharpScriptEngine.Execute("new ScriptedClass().DoPrint()");
+            if (ret.ToString() == data)
+            {
+                return "Oikein";
+            }
+            else
+            {
+                return "Pieleen meni";
+            }
+        }
+
+
+    }
+
+
+}
