6fbe66713f4a16d52de1733cd1b14a6be8b0b9b5
J-Niklander <johannan_aw@outlook.com>
2021-09-15 13:29:44 +0300

    KAYTTÄJÄCONTROLLER paikallisesti toimivan koodin lisäys

--- Controllers/KayttajaController.cs
+++ Controllers/KayttajaController.cs
@@ -181,58 +181,88 @@ namespace KoodinenV1.Controllers
             ViewBag.Viesti = "Jokin meni pieleen, yritä uudelleen. Jos ongelma toistuu, ota yhteyttä ylläpitoon.";
             return View();
         }
-        public IActionResult KurssistaAloitetut(int id,int kurssiId)
+        public IActionResult KurssistaAloitetut(int id/*,int kurssiId*/)
         {
+            // TARVITAANKO kurssiId:tä, jos käyttäjällä useampi kurssi kesken? Voiko ottaa [BIND]-> from form, että ottaisi jotenkin profiilin hyppylinkistä kurssin tähän näkymää?
+            
             id = HttpContext.Session.GetInt32("id") ?? 0;
+            Apumetodit am = new Apumetodit(_context);
+            var käyt = am.HaeKäyttäjä(id);
 
-            KoodinenDBContext db = _context;
+            var suoritetutOppitunnit = (from os in _context.OppituntiSuoritus                                   
+                                        join o in _context.Oppituntis on os.OppituntiId equals o.OppituntiId
+                                        join k in _context.Kurssis on o.KurssiId equals k.KurssiId
+                                        join ks in _context.KurssiSuoritus on k.KurssiId equals ks.KurssiId
+                                        join kä in _context.Kayttajas on ks.KayttajaId equals kä.KayttajaId
+                                        where kä.KayttajaId == id && ks.Kesken == true && ks.Kesken != null && os.Kesken == false && os.Kesken != null
+                                        select new Oppitunti { Nimi = o.Nimi, Kuvaus = o.Kuvaus, OppituntiId = o.OppituntiId }).AsNoTracking().Distinct().ToList();
 
-            var käyt = db.Kayttajas.Where(k => k.KayttajaId == id).FirstOrDefault();
-            db.Entry(käyt).Collection(ku => ku.Kurssis).Load(); // kurssit
-            db.Entry(käyt).Collection(ks => ks.KurssiSuoritus).Load(); // kurssisuoritukset
-            db.Entry(käyt).Collection(t => t.TehtavaSuoritus).Load(); // tehtäväsuoritukset
+            var suorOtTehtävät = (from ts in _context.TehtavaSuoritus                                           
+                                  join t in _context.Tehtavas on ts.TehtavaId equals t.TehtavaId
+                                  join o in _context.Oppituntis on t.OppituntiId equals o.OppituntiId
+                                  join os in _context.OppituntiSuoritus on o.OppituntiId equals os.OppituntiId
+                                  join ks in _context.KurssiSuoritus on os.KayttajaId equals ks.KayttajaId
+                                  where ks.KayttajaId == id && ks.Kesken == true && ks.Kesken != null && os.Kesken == false && os.Kesken != null
+                                  select new Tehtava { Nimi = t.Nimi, Kuvaus = t.Kuvaus, OppituntiId = o.OppituntiId }).AsNoTracking().Distinct().ToList();
+
+            var keskenOppitunnit = (from os in _context.OppituntiSuoritus
+                                    join o in _context.Oppituntis on os.OppituntiId equals o.OppituntiId
+                                    join k in _context.Kurssis on o.KurssiId equals k.KurssiId
+                                    join ks in _context.KurssiSuoritus on k.KurssiId equals ks.KurssiId
+                                    join kä in _context.Kayttajas on ks.KayttajaId equals kä.KayttajaId
+                                    where kä.KayttajaId == id && ks.Kesken == true && ks.Kesken != null && os.Kesken == true && os.Kesken != null
+                                    select new Oppitunti { Nimi = o.Nimi, Kuvaus = o.Kuvaus, OppituntiId = o.OppituntiId }).AsNoTracking().Distinct().ToList();
+
+            var keskenOTsuortehtävät = (from t in _context.Tehtavas 
+                                        join o in _context.Oppituntis on t.OppituntiId equals o.OppituntiId
+                                        join os in _context.OppituntiSuoritus on o.OppituntiId equals os.OppituntiId
+                                        join ks in _context.KurssiSuoritus on os.KayttajaId equals ks.KayttajaId
+                                        where ks.KayttajaId == id && ks.Kesken == true && ks.Kesken != null && os.Kesken == true && os.Kesken != null
+                                        select new Tehtava { TehtavaId = t.TehtavaId, Nimi = t.Nimi, Kuvaus = t.Kuvaus, OppituntiId = o.OppituntiId }).AsNoTracking().Distinct().ToList();
+
+            /// TOIMII tällä versiolla, jos tehtäväsuorituksiin lisätty myös Kesken-kenttä:
+            /// var keskenOTsuortehtävät = (from ts in _context.TehtavaSuoritus
+            //var keskenOTsuortehtävät = (from ts in _context.TehtavaSuoritus
+            //                            join t in _context.Tehtavas on ts.TehtavaId equals t.TehtavaId
+            //                            join o in _context.Oppituntis on t.OppituntiId equals o.OppituntiId
+            //                            join os in _context.OppituntiSuoritus on o.OppituntiId equals os.OppituntiId
+            //                            join ks in _context.KurssiSuoritus on os.KayttajaId equals ks.KayttajaId
+            //                            where ks.KayttajaId == id && ks.Kesken == true && ks.Kesken != null && os.Kesken == true && os.Kesken != null && ts.Kesken == false && ts.Kesken != null
+            //                            select new Tehtava { Nimi = t.Nimi, Kuvaus = t.Kuvaus, OppituntiId = o.OppituntiId, TehtavaId = t.TehtavaId }).AsNoTracking().Distinct().ToList();
+
+            // NÄILLÄ NÄKYMIN PRINTTAA HULLUN TOISTOMÄÄRÄN SAMAA SARAKETTA, EI TEE TÄTÄ PAIKALLISELLA DB:LLÄ). SAA NÄKYVIIN KUN KÄY KOMMENTOIMASSA AUKI
+            // PROFIILI CSHTML: RIVIN 89-91 JA KOMMENTOI POIS RIVIT 92-94.
+
+            var käyttäjä = _context.Kayttajas.Where(k => k.KayttajaId == id).FirstOrDefault();
+            _context.Entry(käyttäjä).Collection(ku => ku.Kurssis).Load(); // kurssit
             foreach (var kurssi in käyt.Kurssis)
             {
-                db.Entry(kurssi).Collection(x => x.Oppituntis).Load();
+                _context.Entry(kurssi).Collection(x => x.Oppituntis).Load();
                 foreach (var oppitunti in kurssi.Oppituntis)
                 {
-                    db.Entry(oppitunti).Collection(x => x.Tehtavas).Load();
+                    _context.Entry(oppitunti).Collection(x => x.Tehtavas).Load();
+                }
+            }
+            var kurssinteht = _context.Tehtavas.ToList();
+
+            var keskenOTkeskentehtävät = new List<Tehtava>();
+
+            foreach (var t in kurssinteht)
+            {
+                foreach (var teht in keskenOTsuortehtävät)
+                {
+                    if (teht.TehtavaId != t.TehtavaId)
+                        keskenOTkeskentehtävät.Add(t);
                 }
             }
 
-            //var kurssinimi = (from x in db.Kurssis
-            //                  where x.KurssiId == kurssiId
-            //                  select x.Nimi).FirstOrDefault();
+            ViewBag.tehdytoppit = suoritetutOppitunnit;
+            ViewBag.tehdytteht = suorOtTehtävät;
+            ViewBag.keskenoppit = keskenOppitunnit;
+            ViewBag.keskenOTsuorT = keskenOTsuortehtävät;
+            ViewBag.keskenOTkeskentT = keskenOTkeskentehtävät;
 
-            var kaikkiteht = db.Tehtavas.ToList();
-            var kaikkiopt = db.Oppituntis.ToList();
-
-            var optsuoritus= db.OppituntiSuoritus.Where(x => x.Kesken == false && x.Kesken != null && x.Kesken != true).ToList();
-            var optEIsuoritettu = db.OppituntiSuoritus.Where(o => o.Kesken == true || o.Kesken == null && o.Kesken != false).ToList();
-
-
-            var suoritetutOppit = (from k in kaikkiopt
-                                   join os in optsuoritus on k.OppituntiId equals os.OppituntiId
-                                   select new Oppitunti { Nimi = k.Nimi, Kuvaus = k.Kuvaus }).ToList();
-            var suoritTeht = (from t in kaikkiteht
-                             join s in suoritetutOppit on t.OppituntiId equals s.OppituntiId
-                             select new Tehtava { Nimi = t.Nimi, Kuvaus = t.Kuvaus}).ToList();
-
-           var EIsuoritetutOppit = (from k in kaikkiopt
-                                    join os in optEIsuoritettu on k.OppituntiId equals os.OppituntiId
-                                    select new Oppitunti { Nimi = k.Nimi, Kuvaus = k.Kuvaus }).ToList();
-            var EIsuoritTeht = (from t in kaikkiteht
-                              join s in optEIsuoritettu on t.OppituntiId equals s.OppituntiId
-                              select new Tehtava { Nimi = t.Nimi, Kuvaus = t.Kuvaus }).ToList();
-
-            //ViewBag.kurssinimi = kurssinimi;
-            ViewBag.tehdytoppit = suoritetutOppit; // ok
-            //ViewBag.tehdytteht = suoritTeht;
-            ViewBag.keskenoppit = EIsuoritetutOppit;
-            //ViewBag.keskenteht = EIsuoritTeht;
-
-            //ViewBag.nimi = käyttäjä.Nimi;
-            //ViewBag.id = käyttäjä.KayttajaId;
+            //ViewBag.kurssinimi = kurssinimi; // haettava myös
 
             return View(käyt);
         }
